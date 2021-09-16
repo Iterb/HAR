3a05d24a1ba4ae3578830c54e9c928502e099beb
Sebastian Puchała <sebastian.puchala@gmail.com>
2021-09-16 23:40:54 +0200

    Implemented logging

---
+++ utils/wandblog.py
@@ -0,0 +1,17 @@
+import wandb
+
+
+def setup_wandb_logger(cfg):
+
+    wandb.login()
+    run = wandb.init(project='human_activity_recognition',
+           config={
+              "learning_rate": cfg.SOLVER.BASE_LR,
+              "epochs": cfg.SOLVER.MAX_EPOCHS,
+              "batch_size": cfg.SOLVER.BATCH_SIZE ,
+              "architecture": cfg.MODEL.ARCH,
+              "features_type":cfg.MODEL.FEATURES_TYPE,
+              "number_of_frames": cfg.SEQUENCE.LIN_SIZE,
+              "Test_train_split" : cfg.DATASETS.SPLIT_TYPE,
+           })
+    return run
