e426769a01d269c1086942629ed9d3769a044273
Sebastian Puchała <sebastian.puchala@gmail.com>
2021-09-16 23:40:05 +0200

    Updated config

--- config/defaults.py
+++ config/defaults.py
@@ -7,11 +7,8 @@ _C = CN()
 
 _C.MODEL = CN()
 _C.MODEL.ARCH = "single" # single/double/triple
-
-_C.MODEL.NUM_CLASSES = 6
-_C.MODEL.DROPOUT = 0.25
-_C.MODEL.HIDDEN_NEURONS = 1024
-
+_C.MODEL.NAME = "single_lstm_F1" 
+_C.MODEL.FEATURES_TYPE = '1'
 # -----------------------------------------------------------------------------
 # Config definition
 # -----------------------------------------------------------------------------
@@ -25,10 +22,13 @@ _C.SEQUENCE.LABEL_OFFSET = 0
 _C.DATASETS = CN()
 # CSV of all key points in all videos from NTU dataset
 _C.DATASETS.FULL = ("/media/sebastian/STORAGE_HDD/data/rose_data_pc_2.csv") #
-_C.DATASETS.FEATURES = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
+_C.DATASETS.FEATURES_FULL = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
+# features for double LSTM
+_C.DATASETS.FEATURES2_PER1 = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
+_C.DATASETS.FEATURES2_PER2 = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
 # List of the dataset names for testing
 _C.DATASETS.TEST = ("data/datasets/test.csv")
-_C.DATASETS.SPLIT_TYPE = 'cv' # cv or cs
+_C.DATASETS.SPLIT_TYPE = 'cs' # cv or cs
 _C.DATASETS.TRAIN_SUBJECTS = [1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19, 25, 27, 28, 31, 34, 35, 38]
 _C.DATASETS.TRAIN_CAMERAS = [2,3] 
 # -----------------------------------------------------------------------------
@@ -43,10 +43,9 @@ _C.DATALOADER.NUM_WORKERS = 8
 # ---------------------------------------------------------------------------- #
 _C.SOLVER = CN()
 _C.SOLVER.OPTIMIZER_NAME = "Adam"
-_C.SOLVER.MAX_EPOCHS = 200
+_C.SOLVER.MAX_EPOCHS = 20
 _C.SOLVER.BASE_LR = 0.001
-_C.SOLVER.LOG_PERIOD = 100
-_C.SOLVER.BATCH_SIZE = 1
+_C.SOLVER.BATCH_SIZE = 64
 
 _C.TEST = CN()
 _C.TEST.BATCH_SIZE = 1
