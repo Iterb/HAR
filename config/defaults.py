dab17122ea0685d6255f3cd9d11e66d17d4a7898
Sebastian Puchała <sebastian.puchala@gmail.com>
2021-09-18 00:54:21 +0200

    Config update

--- config/defaults.py
+++ config/defaults.py
@@ -6,9 +6,11 @@ from yacs.config import CfgNode as CN
 _C = CN()
 
 _C.MODEL = CN()
+_C.MODEL.DROPOUT_RATE = 0.2
 _C.MODEL.ARCH = "single" # single/double/triple
 _C.MODEL.NAME = "single_lstm_F1" 
-_C.MODEL.FEATURES_TYPE = '1'
+
+#_C.MODEL.CLASSES = ({'punch':0, 'kicking':1, 'pushing': 2, 'pat on back' : 3, 'point finger' : 4, 'hugging' : 5, 'giving an object' : 6, 'touch pocket' : 7, 'shaking hands' : 8, 'walking towards' : 9, 'walking apart' :10})
 # -----------------------------------------------------------------------------
 # Config definition
 # -----------------------------------------------------------------------------
@@ -20,30 +22,35 @@ _C.SEQUENCE.LABEL_OFFSET = 0
 # Dataset
 # -----------------------------------------------------------------------------
 _C.DATASETS = CN()
+_C.DATASETS.FEATURES_TYPE = 2
 # CSV of all key points in all videos from NTU dataset
 _C.DATASETS.FULL = ("/media/sebastian/STORAGE_HDD/data/rose_data_pc_2.csv") #
 _C.DATASETS.FEATURES_FULL = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
+_C.DATASETS.FEATURES2_FULL = ("/home/sebastian/projects/HAR/polarcoords_pera.csv")
+_C.DATASETS.FEATURES3_FULL = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
 # features for double LSTM
-_C.DATASETS.FEATURES2_PER1 = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
-_C.DATASETS.FEATURES2_PER2 = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
+_C.DATASETS.FEATURES_PER1_D = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
+_C.DATASETS.FEATURES_PER2_D = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
+_C.DATASETS.FEATURES2_PER1_D = ("/media/sebastian/STORAGE_HDD/data/polarcoords_pera.csv")
+_C.DATASETS.FEATURES2_PER2_D = ("/media/sebastian/STORAGE_HDD/data/polarcoords_perb.csv")
+_C.DATASETS.FEATURES3_PER1_D = ("/media/sebastian/STORAGE_HDD/data/polarcoords2_pera.csv")
+_C.DATASETS.FEATURES3_PER2_D = ("/media/sebastian/STORAGE_HDD/data/polarcoords2_perb.csv")
+# features for triple LSTM
+_C.DATASETS.FEATURES_PER1 = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
+_C.DATASETS.FEATURES_PER2 = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
+_C.DATASETS.FEATURES_DIST = ("/media/sebastian/STORAGE_HDD/data/normalized_25P_short.csv")
 # List of the dataset names for testing
 _C.DATASETS.TEST = ("data/datasets/test.csv")
 _C.DATASETS.SPLIT_TYPE = 'cs' # cv or cs
 _C.DATASETS.TRAIN_SUBJECTS = [1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19, 25, 27, 28, 31, 34, 35, 38]
 _C.DATASETS.TRAIN_CAMERAS = [2,3] 
-# -----------------------------------------------------------------------------
-# DataLoader
-# -----------------------------------------------------------------------------
-_C.DATALOADER = CN()
-# Number of data loading threads
-_C.DATALOADER.NUM_WORKERS = 8
 
 # ---------------------------------------------------------------------------- #
 # Solver
 # ---------------------------------------------------------------------------- #
 _C.SOLVER = CN()
 _C.SOLVER.OPTIMIZER_NAME = "Adam"
-_C.SOLVER.MAX_EPOCHS = 20
+_C.SOLVER.MAX_EPOCHS = 55
 _C.SOLVER.BASE_LR = 0.001
 _C.SOLVER.BATCH_SIZE = 64
 
--- utils/wandblog.py
+++ utils/wandblog.py
@@ -10,8 +10,9 @@ def setup_wandb_logger(cfg):
               "epochs": cfg.SOLVER.MAX_EPOCHS,
               "batch_size": cfg.SOLVER.BATCH_SIZE ,
               "architecture": cfg.MODEL.ARCH,
-              "features_type":cfg.MODEL.FEATURES_TYPE,
+              "features_type":cfg.DATASETS.FEATURES_TYPE,
               "number_of_frames": cfg.SEQUENCE.LIN_SIZE,
               "Test_train_split" : cfg.DATASETS.SPLIT_TYPE,
+              "Dropout": cfg.MODEL.DROPOUT_RATE
            })
     return run
