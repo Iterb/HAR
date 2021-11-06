2fbb0321c5fd66de185b0dd7cb02288910b2923c
Sebastian Puchała <sebastian.puchala.95@gmail.com>
2021-11-06 23:50:18 +0100

    Inference for openpose pipeline

--- engine/inference.py
+++ engine/inference.py
@@ -8,6 +8,8 @@ import tensorflow as tf
 import numpy as np
 from sklearn import preprocessing
 from sklearn.impute import SimpleImputer
+import onnx
+import onnxruntime as rt
 import pandas as pd
 FEATURES_TYPE_1 = 1
 FEATURES_TYPE_2 = 2
@@ -16,66 +18,78 @@ SINGLE_LSTM = 'single'
 DOUBLE_LSTM = 'double'
 TRIPLE_LSTM = 'triple'
 
-def do_test(
+def do_inference(
         cfg,
-        model,
         data,
 ):
-    logger = logging.getLogger("model.test")
-    logger.info("Start testing")
-    X_train_seq, y_train_seq, X_test_seq, y_test_seq = data
+    features = preprocess(cfg, data)
+    logger = logging.getLogger("model.infer")
+    logger.info("Start inferencing")
 
-    score = model.evaluate(X_test_seq, y_test_seq, verbose=1)
-
-    wandb.log({'Best_val_acc': score[1]})
+    m = rt.InferenceSession(cfg.INFER.MODEL_PATH)
+    input_name = m.get_inputs()[0].name
+    label_name = m.get_outputs()[0].name
+    onnx_pred = m.run([label_name], {input_name: features[0].astype(np.float32)})
+    class_dict = {0 : 'punch', 1 : 'kicking', 2 : 'pushing', 3 : 'pat on back', 4 : 'point finger',
+              5 : 'hugging', 6 : 'giving an object', 7 : 'touch pocket',
+              8 : 'shaking hands', 9 : 'walking towards', 10 : 'walking apart'}
+    preds = np.argmax((onnx_pred), axis=2)
+    print(preds)
+    for p in preds[0]:
+        print(class_dict[p])
     logger.info('Finished Testing')
 
-
-
-    return score
 
 def preprocess(cfg, data):
     scaler = load(open(f'sklearn/{cfg.MODEL.ARCH}_F{cfg.DATASETS.FEATURES_TYPE}_scaler.pkl', 'rb'))
     imputer = load(open(f'sklearn/{cfg.MODEL.ARCH}_F{cfg.DATASETS.FEATURES_TYPE}_imputer.pkl', 'rb'))
-
+    seq_type = 'linspace'
     if cfg.INFER.ARCH == SINGLE_LSTM:
-        features = data
+        features = data[0]
+        print(features.shape)
         features =  pd.DataFrame(imputer.transform(features), columns=features.columns, index=features.index)
         features = pd.DataFrame(scaler.transform(features), columns=features.columns, index=features.index)
-        temporal_features = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features)
+        temporal_features = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features, seq_type)
         return [temporal_features]
     if cfg.INFER.ARCH == DOUBLE_LSTM:
-        features_per1, features_per2 = data
+        features_per1, features_per2 = data[0]
         features_per1 =  pd.DataFrame(imputer.transform(features_per1), columns=features_per1.columns, index=features_per1.index)
         features_per1 = pd.DataFrame(scaler.transform(features_per1), columns=features_per1.columns, index=features_per1.index)
         features_per2 =  pd.DataFrame(imputer.transform(features_per2), columns=features_per2.columns, index=features_per2.index)
         features_per2 = pd.DataFrame(scaler.transform(features_per2), columns=features_per2.columns, index=features_per2.index)
-        temporal_features_per1 = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features_per1)
-        temporal_features_per2 = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features_per2)
+        temporal_features_per1 = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features_per1, seq_type)
+        temporal_features_per2 = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features_per2, seq_type)
         return [temporal_features_per1, temporal_features_per2]
     if cfg.INFER.ARCH == TRIPLE_LSTM:
         scaler_dist = load(open(f'sklearn/{cfg.MODEL.ARCH}_F{cfg.DATASETS.FEATURES_TYPE}_scaler_dist.pkl', 'rb'))
         imputer_dist = load(open(f'sklearn/{cfg.MODEL.ARCH}_F{cfg.DATASETS.FEATURES_TYPE}_imputer_dist.pkl', 'rb'))
-        features_per1, features_per2, dist = data
+        features_per1, features_per2, dist = data[0]
         features_per1 =  pd.DataFrame(imputer.transform(features_per1), columns=features_per1.columns, index=features_per1.index)
         features_per1 = pd.DataFrame(scaler.transform(features_per1), columns=features_per1.columns, index=features_per1.index)
         features_per2 =  pd.DataFrame(imputer.transform(features_per2), columns=features_per2.columns, index=features_per2.index)
         features_per2 = pd.DataFrame(scaler.transform(features_per2), columns=features_per2.columns, index=features_per2.index)
         dist = pd.DataFrame(imputer_dist.transform(dist), columns=dist.columns, index=dist.index)
         dist = pd.DataFrame(scaler_dist.transform(dist), columns=dist.columns, index=dist.index)
-        temporal_features_per1 = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features_per1)
-        temporal_features_per2 = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features_per2)
-        temporal_features_dist = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, dist)
+        temporal_features_per1 = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features_per1, seq_type)
+        temporal_features_per2 = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, features_per2, seq_type)
+        temporal_features_dist = create_sequence_for_infer(cfg.INFER.WINDOW_SIZE, dist, seq_type)
         return [temporal_features_per1, temporal_features_per2, temporal_features_dist]
-        
-def create_sequence_for_infer(window_size, df):
 
-    sequential_data = []
-    prev_poses_data = deque(maxlen = window_size)
+def create_sequence_for_infer(window_size, df, seq_type):
+
+    if seq_type == 'window':
+        sequential_data = []
+        prev_poses_data = deque(maxlen = window_size)
         #print (df)
-    for i in df.values:
-        prev_poses_data.append(i)
-        if len(prev_poses_data) == window_size:
-            sequential_data.append(np.array(prev_poses_data))
-
-    return np.array(sequential_data)
+        for i in df.values:
+            prev_poses_data.append(i)
+            if len(prev_poses_data) == window_size:
+                sequential_data.append(np.array(prev_poses_data))           
+        return np.array(sequential_data)
+    elif seq_type == 'linspace':
+        sequential_data = []
+        idx = np.round(np.linspace(0, len(df) - 1, window_size)).astype(int)
+        print(idx)
+        df = df.iloc[idx, :].reset_index(drop=True)
+        sequential_data.append(np.array(df))    
+        return np.array(sequential_data)
